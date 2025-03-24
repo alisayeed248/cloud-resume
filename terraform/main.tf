9fcdc9e72ffce91944216ab918546d3a7a86ce18
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-03-24 14:46:14 -0400

    Edited Terraform to use an existing bucket rather than create a new one and testing deploy.yml.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,15 @@
-# main.tf 
-
 provider "aws" {
   region = "us-east-1"
 }
 
-# S3 bucket that hosts resume
-resource "aws_s3_bucket" "website_bucket" {
+# Use data source to reference existing bucket
+data "aws_s3_bucket" "website_bucket" {
   bucket = "sayeedali-resumesite-prod"
-
-  lifecycle {
-    prevent_destroy = true
-  }
 }
 
 # Disable block public access settings for this specific bucket
 resource "aws_s3_bucket_public_access_block" "website_access" {
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = data.aws_s3_bucket.website_bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -25,7 +19,7 @@ resource "aws_s3_bucket_public_access_block" "website_access" {
 
 # Set bucket ownership controls
 resource "aws_s3_bucket_ownership_controls" "website_ownership" {
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = data.aws_s3_bucket.website_bucket.id
 
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -39,13 +33,13 @@ resource "aws_s3_bucket_acl" "website_bucket_acl" {
     aws_s3_bucket_ownership_controls.website_ownership
   ]
 
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = data.aws_s3_bucket.website_bucket.id
   acl    = "public-read"
 }
 
 # Bucket configuration for website hosting
 resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = data.aws_s3_bucket.website_bucket.id
 
   index_document {
     suffix = "index.html"
@@ -60,7 +54,7 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
 resource "aws_s3_bucket_policy" "website_policy" {
   depends_on = [aws_s3_bucket_public_access_block.website_access]
 
-  bucket = aws_s3_bucket.website_bucket.id
+  bucket = data.aws_s3_bucket.website_bucket.id
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -69,7 +63,7 @@ resource "aws_s3_bucket_policy" "website_policy" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+        Resource  = "${data.aws_s3_bucket.website_bucket.arn}/*"
       }
     ]
   })
