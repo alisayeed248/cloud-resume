4f10c43538fbd1ece6bf7cec72c625c3928a3ec9
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-03-24 14:00:28 -0400

    Added Terraform code to make S3 bucket + index.html to be copied to S3 for hosting

---
+++ terraform/main.tf
@@ -0,0 +1,77 @@
+# main.tf 
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# S3 bucket that hosts resume
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "sayeedali-resumesite-prod"
+}
+
+# Disable block public access settings for this specific bucket
+resource "aws_s3_bucket_public_access_block" "website_access" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Set bucket ownership controls
+resource "aws_s3_bucket_ownership_controls" "website_ownership" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# Make the bucket ACL public-read
+resource "aws_s3_bucket_acl" "website_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.website_access,
+    aws_s3_bucket_ownership_controls.website_ownership
+  ]
+
+  bucket = aws_s3_bucket.website_bucket.id
+  acl    = "public-read"
+}
+
+# Bucket configuration for website hosting
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+# Bucket policy that allows public access
+resource "aws_s3_bucket_policy" "website_policy" {
+  depends_on = [aws_s3_bucket_public_access_block.website_access]
+
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+# Output values
+output "website_url" {
+  value = aws_s3_bucket_website_configuration.website_config.website_endpoint
+}
