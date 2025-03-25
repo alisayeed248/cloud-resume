221806a8c383fd104de8daa548b6cc9d8bead1a9
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-03-25 13:42:16 -0400

    Testing new deploy.yml and Terraform code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <div className="p-8 bg-white shadow-md rounded-lg text-center">
         <h1 className="text-3xl font-bold text-blue-600">
-          Welcome to My Site
+          Welcome to My Dev Site
         </h1>
         <p className="mt-4 text-gray-700">
           Deployed with React, Tailwind, S3, and GitHub Actions
