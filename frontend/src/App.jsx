03f5821c05c8abe00774fb5375e8b531d959aa7c
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-03-25 14:17:01 -0400

    Changed title of tab. Edited main screen viewport to center text.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,8 @@ import './index.css'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="p-8 bg-white shadow-md rounded-lg text-center">
-        <h1 className="text-3xl font-bold text-blue-600">
-          Welcome to My Test Dev Site
-        </h1>
-        <p className="mt-4 text-gray-700">
-          Deployed with React, Tailwind, S3, and GitHub Actions
-        </p>
-      </div>
+    <div className="flex items-center justify-center h-screen w-screen">
+      <h1 className="text-3xl font-bold text-white">Hello, World!</h1>
     </div>
   )
 }
