7c278007f71c7d806123111be530a2758a5c749a
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-03-25 15:24:15 -0400

    Changed font of navbar, added some more sections, added a profile picture.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import resumeImage from "../assets/resume_picture.jpg";
 
 const Navbar = () => {
   return (
-    <div className="w-64 bg-gray-800 text-white p-4 flex flex-col items-center justify-center h-screen">
-      <h2 className="text-xl font-bold mb-8 text-center">My Resume</h2>
-      <ul className="flex flex-col items-center space-y-6 text-lg">
+    <div className="w-72 bg-gray-800 text-white p-4 flex flex-col items-center justify-center h-screen">
+      <a href="#about">
+        <div className="w-40 h-40 rounded-full overflow-hidden mb-8">
+          <img
+            src={resumeImage}
+            alt="Sayeed Ali"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </a>
+
+      <ul className="font-mono flex flex-col items-center space-y-6 text-lg">
         <li>
-          <a href="#about">ABOUT</a>
+          <a href="#about">ABOUT ME</a>
         </li>
         <li>
           <a href="#education">EDUCATION</a>
@@ -16,6 +26,15 @@ const Navbar = () => {
         </li>
         <li>
           <a href="#projects">PROJECTS</a>
+        </li>
+        <li>
+          <a href="#hobbies">HOBBIES</a>
+        </li>
+        <li>
+          <a href="#BLOG">MY BLOG</a>
+        </li>
+        <li>
+          <a href="#contact">CONTACT</a>
         </li>
       </ul>
     </div>
