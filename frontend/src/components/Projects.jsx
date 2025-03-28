cf5d15f430407f4b87f4f07ec98ce74f5bfccad2
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-03-28 14:24:59 -0400

    Added basic description of projects to be mapped to divs.

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Projects = () => {
-  return <div>Projects</div>;
+  const projects = [
+    {
+      id: 1,
+      title: "Resume Website",
+      description:
+        "A portfolio website built with React, Tailwind, and AWS. This project shows the end-to-end development of a serverless Resume website, using S3 to host the bucket, API Gateway and Lambda to handle the blog, Terraform and GitHub Actions for CI/CD, and more. Check it out!",
+    },
+    {
+      id: 2,
+      title: "Music Playlister",
+      description:
+        "A tool made in C# and .NET to grab YouTube videos from their link using the YouTube API and being able to convert and download them as MP3s or MP4s, repeat favorite sections with a slider, and keep a customized profile of songs you liked.",
+    },
+  ];
+
+  return <div></div>;
 };
 
 export default Projects;
