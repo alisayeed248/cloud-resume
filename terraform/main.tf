996f71eb921aaf077b98291e7db11de7c647173d
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-03-31 22:26:55 -0400

    Removed lambda role fields; not needed if existing role.

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,19 +77,6 @@ data "aws_dynamodb_table" "blog_posts_table" {
 # IAM role for Lambda functions
 data "aws_iam_role" "lambda_role" {
   name = "blog_lambda_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
 }
 
 # Check if the policy already exists
