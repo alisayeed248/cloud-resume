136c7f91d7e7c1187ab5b7dbd5166730ec6b7575
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2025-05-30 14:36:10 -0400

    Made scroll activate percentage based loading.

--- frontend/src/components/ScrollProgressBar.jsx
+++ frontend/src/components/ScrollProgressBar.jsx
@@ -11,19 +11,29 @@ const ScrollProgressBar = () => {
         document.documentElement.scrollHeight - window.innerHeight;
 
       const progress = (scrollTop / documentHeight) * 100;
-      console.log({scrollTop, documentHeight, progress: Math.round(progress)});
+      const finalProgress = Math.min(Math.max(progress, 0), 100);
+      setScrollProgress(finalProgress);
+
+      console.log("Progress:", Math.round(finalProgress));
     };
     // whenever scroll event is detected, we run the function and take a look and check our posiion
     window.addEventListener("scroll", handleScroll);
+    handleScroll(); // we call it to set its initial state
     return () => window.removeEventListener("scroll", handleScroll);
     // only once when component first mounts, not everytime it re-renders
   }, []);
 
-
   return (
     /* Top left, spans the entire width, 16px (h-4) so easy to see z-50 so on top of everything*/
-    <div className="fixed top-0 left-0 w-full h-4 bg-red-500 z-50">
-      <p className="text-white text-xs">Check console for scroll math</p>
+    <div className="fixed top-0 left-0 w-full h-8 bg-gray-300 z-50">
+      <div
+        className="h-full bg-red-500"
+        style={{ width: `${scrollProgress}%` }}
+      >
+        <span className="text-white text-xs p-1">
+          {Math.round(scrollProgress)}%
+        </span>
+      </div>
     </div>
   );
 };
